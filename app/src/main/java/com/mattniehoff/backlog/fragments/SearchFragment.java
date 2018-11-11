166025fc46d8a5764d20f95f51e164a06dc8cd2d
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-11-11 16:26:18 -0600

    Sets up EditText and ImageButton for actually searching.

--- app/src/main/java/com/mattniehoff/backlog/fragments/SearchFragment.java
+++ app/src/main/java/com/mattniehoff/backlog/fragments/SearchFragment.java
@@ -1,20 +1,30 @@
 package com.mattniehoff.backlog.fragments;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mattniehoff.backlog.R;
 import com.mattniehoff.backlog.viewmodels.SearchViewModel;
 
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel mViewModel;
+    private SearchViewModel searchViewModel;
+    private EditText editText;
+    private ImageButton searchImageButton;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -23,13 +33,48 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.search_fragment, container, false);
+
+        setupImageSearchButton(rootView);
+        setupEditText(rootView);
+
+        return rootView;
+    }
+
+    private void setupEditText(View rootView) {
+        // https://stackoverflow.com/a/19217624/2107568
+        editText = rootView.findViewById(R.id.search_edittext);
+        editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    searchImageButton.performClick();
+
+                    // http://tonylukasavage.com/blog/2011/06/02/android-quick-tip--edittext-with-done-button-that-closes-the-keyboard/
+                    InputMethodManager imm = (InputMethodManager)view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void setupImageSearchButton(View rootView) {
+        searchImageButton = rootView.findViewById(R.id.search_image_button);
+        searchImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Search for " + editText.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         // TODO: Use the ViewModel
     }
 
