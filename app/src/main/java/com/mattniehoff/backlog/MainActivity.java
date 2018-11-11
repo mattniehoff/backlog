7bab85696381ad3c80538cbacf79b1f39a47e926
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-11-11 12:23:56 -0600

    Sets up navigation menu for 4 main modules and implements listener.

--- app/src/main/java/com/mattniehoff/backlog/MainActivity.java
+++ app/src/main/java/com/mattniehoff/backlog/MainActivity.java
@@ -7,29 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_search:
+                mTextMessage.setText(R.string.title_nav_search);
+                return true;
+            case R.id.navigation_library:
+                mTextMessage.setText(R.string.title_nav_library);
+                return true;
+            case R.id.navigation_backlog:
+                mTextMessage.setText(R.string.title_nav_backlog);
+                return true;
+            case R.id.navigation_statistics:
+                mTextMessage.setText(R.string.title_nav_statistics);
+                return true;
+        }
+
+        return false;
+    }
 }
