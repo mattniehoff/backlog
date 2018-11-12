a15a0a1f9d88a4aec7ab82a34e85386a11caf43e
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-11-12 16:42:48 -0600

    Adds picasso for loading images into views.
    Sets sample data for the library recyclerview. Proving it works...ish.

--- app/src/main/java/com/mattniehoff/backlog/adapters/LibraryAdapter.java
+++ app/src/main/java/com/mattniehoff/backlog/adapters/LibraryAdapter.java
@@ -6,16 +6,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mattniehoff.backlog.R;
 import com.mattniehoff.backlog.model.database.GameEntry;
 import com.mattniehoff.backlog.viewmodels.LibraryViewModel;
+import com.squareup.picasso.Picasso;
 
 import java.util.Date;
 import java.util.List;
 
-public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder>  {
+public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder> {
 
     private List<GameEntry> gameEntryList;
 
@@ -40,6 +42,15 @@ public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryV
 
         libraryViewHolder.gameTitleTextView.setText(gameEntry.getName());
         libraryViewHolder.gameIdTextView.setText("Game ID: " + gameEntry.getId());
+
+        String gameCoverUrl = gameEntry.getCoverUrl();
+        if (gameCoverUrl.length() > 0) {
+            Picasso.get()
+                    .load(gameCoverUrl)
+//                    .placeholder(R.drawable.ic_videogame_asset_black_24dp)
+                    .error(R.drawable.ic_videogame_asset_black_24dp)
+                    .into(libraryViewHolder.gameCoverImageView);
+        }
     }
 
     @Override
@@ -52,7 +63,7 @@ public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryV
     }
 
     public void setGameEntryList(final List<GameEntry> newGameEntryList) {
-        newGameEntryList.add(new GameEntry(1, "The first game", new Date(), null, "images.igdb.com/igdb/image/upload/t_thumb/f9jvrf3nwdgdil287sla.jpg"));
+        newGameEntryList.add(new GameEntry(1, "The first game", new Date(), null, "http://images.igdb.com/igdb/image/upload/t_thumb/f9jvrf3nwdgdil287sla.jpg"));
         newGameEntryList.add(new GameEntry(2, "The second game", new Date(), null, "images.igdb.com/igdb/image/upload/t_thumb/f9jvrf3nwdgdil287sla.jpg"));
 
         gameEntryList = newGameEntryList;
@@ -62,12 +73,14 @@ public class LibraryAdapter extends RecyclerView.Adapter<LibraryAdapter.LibraryV
     class LibraryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         final TextView gameTitleTextView;
         final TextView gameIdTextView;
+        final ImageView gameCoverImageView;
 
         LibraryViewHolder(View view) {
             super(view);
 
             gameTitleTextView = view.findViewById(R.id.library_list_item_game_title_text_view);
             gameIdTextView = view.findViewById(R.id.library_list_item_game_id_text_view);
+            gameCoverImageView = view.findViewById(R.id.library_list_item_cover_image_view);
 
             view.setOnClickListener(this);
         }
