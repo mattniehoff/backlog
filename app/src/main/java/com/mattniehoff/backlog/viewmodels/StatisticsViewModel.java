95f768985aa915b4bbda90ffdc80176302e512d6
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-11-14 03:48:08 -0600

    Creates basic statistics viewmodel/fragment.

--- app/src/main/java/com/mattniehoff/backlog/viewmodels/StatisticsViewModel.java
+++ app/src/main/java/com/mattniehoff/backlog/viewmodels/StatisticsViewModel.java
@@ -1,7 +1,24 @@
 package com.mattniehoff.backlog.viewmodels;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
+import com.mattniehoff.backlog.model.database.GameEntry;
+import com.mattniehoff.backlog.repository.GameRepository;
+
+import java.util.List;
+
 public class StatisticsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final GameRepository repository;
+    private final LiveData<List<GameEntry>> gameList;
+
+    public StatisticsViewModel(GameRepository repository) {
+        this.repository = repository;
+        this.gameList = this.repository.getGameLibrary();
+    }
+
+    public LiveData<List<GameEntry>> getGameLiibrary() {
+        return gameList;
+    }
 }
+
